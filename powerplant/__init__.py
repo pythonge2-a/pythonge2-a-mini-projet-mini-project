4f4d7ebbf3e6d5eab459e18668ae4326019269c2
porretleo <leo.porret@heig-vd.ch>
2024-12-09 14:15:44 +0100

    ajout class KwhPrice

--- powerplant/__init__.py
+++ powerplant/__init__.py
@@ -1,3 +1,6 @@
+#importer vos classes ici "from nom_du_fichier import nom_de_la_classe"
+from .price import KwhPrice
+
 class Ouga:
     def __init__(self):
-        print("oui")
+        print("yeah ça merche")
--- powerplant/__main__.py
+++ powerplant/__main__.py
@@ -1,8 +1,10 @@
-from powerplant import Ouga
+from powerplant import Ouga # classe test
+
+#inclure les classes ici "from powerplant.nom_du_fichier import nom_de_la_classe"
+from powerplant.price import KwhPrice 
 
 def main():
-    t = Ouga()
-    
+    test = Ouga()
 
 if __name__ == "__main__":
     main()
