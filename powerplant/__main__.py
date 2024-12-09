360aaa6e6ecde222b3507d8d499cbd895d9e0723
Yann Dos Santos <yann@MacBook-Pro-de-Yann.local>
2024-12-09 16:51:21 +0100

    Added classes folder

--- powerplant/__main__.py
+++ powerplant/__main__.py
@@ -1,7 +1,7 @@
 from powerplant import Ouga # classe test
 
 #inclure les classes ici "from powerplant.nom_du_fichier import nom_de_la_classe"
-from powerplant.price import KwhPrice 
+from powerplant.classes.price import KwhPrice 
 
 def main():
     test = Ouga()
