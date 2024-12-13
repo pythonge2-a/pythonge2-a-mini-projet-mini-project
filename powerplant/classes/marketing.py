b225c914366d9e74315a26b71de929d6042761fb
Yann Dos Santos <yann@MacBook-Pro-de-Yann.local>
2024-12-13 09:01:31 +0100

    updated marketing class

--- powerplant/classes/marketing.py
+++ powerplant/classes/marketing.py
@@ -10,5 +10,14 @@ class marketing:
     def get_user_price(self):   #retourne le prix de vente utilisateur
         return self.user_price
     
-    def get_user_gain(self, demand):    #retourne les gains utilisateur par seconde
+    def get_user_bank(self):    #retourne la valeur de la banque utilisateur
+        return self.bank
+    
+    def get_user_gain(self, demand, storage):    #retourne les gains utilisateur par seconde
+        if storage == 0:
+            return 0
         return self.user_price * demand
+    
+    def update_user_bank(self, gain):
+        self.bank += gain
+        return self.bank
