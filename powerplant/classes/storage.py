08418629df6a176ad8466f7ac66c4bab0d5be5be
porretleo <leo.porret@heig-vd.ch>
2024-12-20 17:53:45 +0100

    update prod

---
+++ powerplant/classes/storage.py
@@ -0,0 +1,17 @@
+# gère le stockage de l'énergie produite par les différentes sources d'énergie
+
+class storage :
+    def __init__(self):
+        self.stock = 0
+        self.stock_max = 1000
+    
+    def add_stock(self, stk):
+        self.stock += stk
+        if self.stock > self.stock_max:
+            self.stock = self.stock_max
+        return self.stock
+    
+    def set_stock_max(self, max):
+        self.stock_max = max
+        return self.stock_max
+    
