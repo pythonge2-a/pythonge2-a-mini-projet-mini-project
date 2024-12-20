7915a9410df72cf2633bee4c339683e50845a564
Yann Dos Santos <yann@MacBook-Pro-de-Yann.local>
2024-12-20 08:50:38 +0100

    added color palette

--- powerplant/classes/__init__.py
+++ powerplant/classes/__init__.py
@@ -1,5 +1,12 @@
 # Importation de tous les modules
 # Syntaxe : from.fichier import *
 
+from .colors import *
+from .dynamo import *
+#from .interface import *
+from .invest import *
+#from .jeux_cable import *
+#from .jeux_sinus import *
+from .marketing import *
 from .price import *
-from .jeux_cable import *
+from .production import *
---
+++ powerplant/classes/colors.py
@@ -0,0 +1,13 @@
+# Définition des couleurs
+# Palette Gruvbox : https://github.com/morhetz/gruvbox
+
+bg_color = "#282828"
+fg_color = "#ebdbb2"
+red = "#cc241d"
+green = "#98971a"
+yellow = "#d79921"
+blue = "#458588"
+purple = "#b16286"
+aqua = "#689d6a"
+orange = "#d65d0e"
+gray = "#928374"
