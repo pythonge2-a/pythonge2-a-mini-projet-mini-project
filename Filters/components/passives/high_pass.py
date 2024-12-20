b88457e89fc9e0cd9cdf913c1facdd4f3af8a5e0
Maxime Magnenat <maxime.magnenat@heig-vd.ch>
2024-12-20 09:58:59 +0100

    passe haut ordre 1 et 2

--- Filters/components/passives/high_pass.py
+++ Filters/components/passives/high_pass.py
@@ -3,18 +3,49 @@ import math
 
 class HighPassFilter:
     @staticmethod
-    def cutoff_frequency(R, L, C):
+    def cutoff_frequency_rc(R, C):
+        """
+        Calculate the cutoff frequency of a high-pass RC filter.
+
+        Parameters:
+            R (float): Resistance in ohms.
+            C (float): Capacitance in farads.
+
+        Returns:
+            float: Cutoff frequency in Hz.
+        """
+        if R <= 0 or C <= 0:
+            raise ValueError("Resistance and capacitance must be greater than zero.")
+        return 1 / (2 * math.pi * R * C)
+
+    @staticmethod
+    def cutoff_frequency_rl(R, L):
+        """
+        Calculate the cutoff frequency of a high-pass RL filter.
+
+        Parameters:
+            R (float): Resistance in ohms.
+            L (float): Inductance in henries.
+
+        Returns:
+            float: Cutoff frequency in Hz.
+        """
+        if R <= 0 or L <= 0:
+            raise ValueError("Resistance and inductance must be greater than zero.")
+        return R / (2 * math.pi * L)
+
+    @staticmethod
+    def cutoff_frequency_rlc(L, C):
         """
         Calculate the cutoff frequency of a high-pass RLC filter.
 
         Parameters:
-            R (float): Resistance in ohms.
             L (float): Inductance in henries.
             C (float): Capacitance in farads.
 
         Returns:
             float: Cutoff frequency in Hz.
         """
-        if L == 0:
-            raise ValueError("Inductance must be non-zero.")
-        return R / (2 * math.pi * L)
+        if L <= 0 or C <= 0:
+            raise ValueError("Inductance and capacitance must be greater than zero.")
+        return 1 / (2 * math.pi * math.sqrt(L * C))
