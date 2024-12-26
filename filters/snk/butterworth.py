a0475fb4279e6a4aa2b5f05f853ee73832dbcb98
francisco-oliveirabarbosa <francisco.oliveirabarbosa@heig-vd.ch>
2024-12-26 17:27:34 +0100

    Organisation pt 2

--- filters/snk/butterworth.py
+++ filters/snk/butterworth.py
@@ -1,5 +1,28 @@
-import numpy as np
 import math
-from scipy.signal import TransferFunction as Hjw
-import pandas as pd
 
+class LowPass:
+    def __init__(self,order,cutoff_frequency):
+        self.order = order
+        self.cutoff_frequency = cutoff_frequency
+
+    def transfer_function(self, frequency):
+        w0 = 2 * math.pi * self.cutoff_frequency
+        return 1 / math.sqrt(1 + (frequency / w0) ** (2*self.order))
+    
+class HighPass:
+    def __init__(self,order,cutoff_frequency):
+        self.order = order
+        self.cutoff_frequency = cutoff_frequency
+
+    def transfer_function(self, frequency):
+        w0 = 2 * math.pi * self.cutoff_frequency
+        return 1 / math.sqrt(1 + (frequency / w0) ** (2*self.order))
+
+class BandPass:
+    def __init__(self,order,cutoff_frequency):
+        self.order = order
+        self.cutoff_frequency = cutoff_frequency
+
+    def transfer_function(self, frequency):
+        w0 = 2 * math.pi * self.cutoff_frequency
+        return 1 / math.sqrt(1 + (frequency / w0) ** (2*self.order))
