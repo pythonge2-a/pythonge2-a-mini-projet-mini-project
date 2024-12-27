7d3443eb4e209797e3416525d3eb4372b8f6f8fa
francisco-oliveirabarbosa <francisco.oliveirabarbosa@heig-vd.ch>
2024-12-27 14:55:28 +0100

    Debuts premeteurs

--- filters/snk/__init__.py
+++ filters/snk/__init__.py
@@ -1,5 +1,12 @@
-from .tchebychev import Tchebychev
+#from tchebychev import Tchebychev
+import butterworth as bt
 
 class SallenAndKey:
     def __init__(self):
-        self.tchebychev = Tchebychev()
+        #self.tchebychev = Tchebychev()
+        return None
+    
+    def test_des_functions():
+       bt.Butterworth_LowPass.components(1,"200k")
+       return None
+    
