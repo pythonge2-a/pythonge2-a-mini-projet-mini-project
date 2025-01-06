f290b12b6799bd1402411dfc43f94a1deef2ed7c
esteban-cinelli <esteban.cinelli@heig-vd.ch>
2025-01-06 20:28:51 +0100

    ajout video sur les serie de fourrier (on peut rentrer le nombre de terme et la fonction que l'on souhaite : toothsaw, triangle , square)

--- manim_site/EstebanCinelli/test.py
+++ manim_site/EstebanCinelli/test.py
@@ -177,6 +177,52 @@ class GeometricTransform(Scene):
         self.wait()
 
 
+class FourierSeries(Scene):
+    def construct(self):
+        # Variable pour sélectionner la fonction (square, triangle, toothsaw) et le nombre de termes dans l'approximation
+        function_type = "square"  
+        number_of_terms = 10
+
+        # Ajuster les axes
+        axes = Axes(
+            x_range=[0, 6 * PI, PI / 4],  
+            y_range=[-1.5, 1.5, 0.5],
+            tips=False
+        )
+
+        # Définir les séries de Fourier pour chaque fonction
+        def fourier_series(x, n_terms=10):
+            result = 0
+            if function_type == "square":
+                for n in range(1, n_terms + 1):
+                    if n % 2 != 0:
+                        result += (4 / (PI * n)) * np.sin(n * x)
+            elif function_type == "triangle":
+                for n in range(1, n_terms + 1):
+                    result += (8 / (PI**2 * n**2)) * (-1)**((n-1)//2) * np.sin(n * x)
+            elif function_type == "toothsaw":
+                for n in range(1, n_terms + 1):
+                    result += (2 / (PI * n)) * (-1)**(n+1) * np.sin(n * x)
+            return result
+
+        # Placer le texte en haut au centre
+        wave_label = Text(f"{function_type.capitalize()} Wave, n = {number_of_terms}").to_edge(UP)
+
+        self.play(Create(axes), Write(wave_label))
+
+        # Initialiser l'attribut self.current_term
+        self.current_term = number_of_terms
+
+        series_graph = always_redraw(lambda: axes.plot(lambda x: fourier_series(x, n_terms=self.current_term), color=RED))
+
+        self.add(series_graph)
+
+        for n in range(1, 11):
+            self.current_term = n
+            self.wait(0.5)
+
+        self.wait()
+
 
 
 ## Code test 
@@ -407,3 +453,4 @@ class Integral(Scene):
         self.play(Write(a_label), Write(b_label))
         
         self.wait(1)
+
